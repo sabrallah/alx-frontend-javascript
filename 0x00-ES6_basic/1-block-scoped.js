04d3a57a282705fd1b4000816118765a8bdd6153
sabrallah <sabrallah1991@gmail.com>
2024-01-02 15:58:52 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    task = false;
+    task2 = true;
   }
   return [task, task2];
 }
