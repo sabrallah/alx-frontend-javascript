602679f0dbbed6e6079b238d03f8540f9430bea8
sabrallah <sabrallah1991@gmail.com>
2024-01-03 07:00:24 +0000

    task9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+// Fichier : 9-try.js
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
