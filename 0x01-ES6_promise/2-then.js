2c1c4c5b87e85ae6ec79633a6f940c86d56d77b8
sabrallah <sabrallah1991@gmail.com>
2024-01-03 05:43:10 +0000

    task2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+// Fichier : 2-then.js
+
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
