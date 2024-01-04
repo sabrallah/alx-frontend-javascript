4da248dbe540e376a7879099363165d93400015a
sabrallah <sabrallah1991@gmail.com>
2024-01-04 13:57:34 +0000

    task10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,27 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor();
+  }
+
+  get brand() {
+    return this[_brand];
+  }
+
+  get motor() {
+    return this[_motor];
+  }
+
+  get color() {
+    return this[_color];
   }
 }
